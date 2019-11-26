02d90786c3e365575361f8845bc639ff5c7c8379
yutayamazaki <tppymd@gmail.com>
2019-11-27 01:25:46 +0900

    Add mean_squared_error

--- tests/test_losses.py
+++ tests/test_losses.py
@@ -14,3 +14,14 @@ class TestBinaryCrossEntropy(unittest.TestCase):
     def test_return(self):
         bce = losses.binary_cross_entropy(self.y_true, self.y_pred)
         self.assertIsInstance(bce, np.float64)
+
+
+class TestMeanSquaredError(unittest.TestCase):
+
+    def setUp(self):
+        self.y_true = np.array([0, 1])
+        self.y_pred = np.array([0.3, 0.4])
+
+    def test_return(self):
+        mse = losses.mean_squared_error(self.y_true, self.y_pred)
+        self.assertIsInstance(mse, np.float64)
