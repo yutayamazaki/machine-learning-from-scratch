9d64edb7711eefe78c27206770179702b5a82f4f
yutayamazaki <tppymd@gmail.com>
2019-11-26 14:28:56 +0900

    Add examples

---
+++ examples/logistic_regression.py
@@ -0,0 +1,20 @@
+import sys
+
+from sklearn.datasets import load_breast_cancer
+from sklearn.model_selection import train_test_split
+
+sys.path.append('..')
+from mlscratch.logistic_regression import LogisticRegression
+from mlscratch.metrics import accuracy_score
+
+if __name__ == '__main__':
+    X, y = load_breast_cancer(return_X_y=True)
+    X_train, X_test, y_train, y_test = train_test_split(X, y)
+
+    model = LogisticRegression()
+
+    model.fit(X_train, y_train)
+    y_pred = model.predict(X_test)
+
+    acc = accuracy_score(y_test, y_pred)
+    print(f'Accuracy: {acc}')
