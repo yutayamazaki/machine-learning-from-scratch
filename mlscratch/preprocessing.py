694d933669d8d84b157daeaa3155916364ad5c59
yutayamazaki <tppymd@gmail.com>
2019-12-07 15:54:52 +0900

    Add _BaseScaler

--- mlscratch/preprocessing.py
+++ mlscratch/preprocessing.py
@@ -1,7 +1,20 @@
 import numpy as np
 
 
-class MinMaxScaler:
+class _BaseScaler:
+
+    def fit(self, X, y=None):
+        raise NotImplementedError()
+
+    def transform(self, X, y=None):
+        raise NotImplementedError()
+
+    def fit_transform(self, X, y=None):
+        self.fit(X, y)
+        return self.transform(X, y)
+
+
+class MinMaxScaler(_BaseScaler):
 
     def fit(self, X, y=None):
         self.x_min_ = np.min(X, axis=0)
@@ -11,6 +24,13 @@ class MinMaxScaler:
     def transform(self, X, y=None):
         return (X - self.x_min_) / self.x_max_
 
-    def fit_transform(self, X, y=None):
-        self.fit(X, y)
-        return self.transform(X, y)
+
+class StandardScaler(_BaseScaler):
+
+    def fit(self, X, y=None):
+        self.x_std_ = np.std(X, axis=0)
+        self.x_mean_ = np.mean(X, axis=0)
+        return self
+
+    def transform(self, X, y=None):
+        return (X - self.x_mean_) / self.x_std_
