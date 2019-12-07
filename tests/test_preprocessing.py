997b39f38342b2027c9886744165c36bd5bfe5ef
yutayamazaki <tppymd@gmail.com>
2019-12-07 15:55:19 +0900

    Add StandardScaler and its tests

--- tests/test_preprocessing.py
+++ tests/test_preprocessing.py
@@ -2,7 +2,7 @@ import unittest
 
 import numpy as np
 
-from mlscratch.preprocessing import MinMaxScaler
+from mlscratch.preprocessing import MinMaxScaler, StandardScaler
 
 
 class TestMinMaxScaler(unittest.TestCase):
@@ -42,4 +42,44 @@ class TestMinMaxScaler(unittest.TestCase):
         """ Check it returns same result as self.fit().transform(). """
         ret = MinMaxScaler().fit_transform(self.X)
         expected = MinMaxScaler().fit(self.X).transform(self.X)
+        self.assertAlmostEqual(ret.all(), expected.all())
+
+
+class TestStandardScaler(unittest.TestCase):
+
+    def setUp(self):
+        self.X = np.array([
+            [0, 1, 2],
+            [10, 20, 30]
+        ])
+
+    def test_fit_return(self):
+        """ Check StandardScaler.fit returns itself. """
+        ret = StandardScaler().fit(self.X)
+        self.assertIsInstance(ret, StandardScaler)
+
+    def test_fit_attributes(self):
+        """ Check new attributes after fitted. """
+        ret = StandardScaler().fit(self.X)
+        expected_x_mean_ = np.array([5., 10.5, 16])
+        self.assertAlmostEqual(ret.x_mean_.all(), expected_x_mean_.all())
+
+        expected_x_std_ = np.array([5., 9.5, 14.])
+        self.assertAlmostEqual(ret.x_std_.all(), expected_x_std_.all())
+
+    def test_transform(self):
+        """ Check return value and its dtype. """
+        ret = StandardScaler().fit(self.X).transform(self.X)
+        self.assertIsInstance(ret, np.ndarray)
+
+        expected = np.array([
+            [-1, -1, -1],
+            [1, 1, 1]
+        ])
+        self.assertAlmostEqual(ret.all(), expected.all())
+
+    def test_fit_transform(self):
+        """ Check it returns same result as self.fit().transform(). """
+        ret = StandardScaler().fit_transform(self.X)
+        expected = StandardScaler().fit(self.X).transform(self.X)
         self.assertAlmostEqual(ret.all(), expected.all())
